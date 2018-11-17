ecc1fb81d1aa597ebaa681a2637efefee9067352
timo <timothy.nibeaudeau@etu.univ-tours.fr>
2018-11-17 16:13:48 +0100

    Initialise project

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm")
+}
+
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.10")
+
+    implementation("io.reactivex.rxjava2:rxjava:2.2.3")
+    implementation("nl.littlerobots.rxlint:rxlint:1.7.0")
+
+    testImplementation("junit:junit:4.12")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ changelog.md
@@ -0,0 +1,14 @@
+## [VERSION-NAME] - DATE 
+### Added
+
+### Changed
+
+### Deprecated
+
+### Removed
+
+### Fixed
+
+### Security
+
+https://semver.org/spec/v2.0.0.html
